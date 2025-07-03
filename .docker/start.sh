b7fc06f0e30ed621fe4cae0f1148fd13993c41a4
DeivisFelipe <guerreirodosul01@gmail.com>
2025-07-03 14:46:44 -0300

    fix: mudando a cadificação do projeto

--- .docker/start.sh
+++ .docker/start.sh
@@ -1,24 +1,28 @@
 #!/bin/sh
 
-echo "[??] Instalando depend�ncias do Laravel..."
+echo "[🔧] Corrigindo permissões em storage e cache..."
+chown -R www-data:www-data storage bootstrap/cache
+chmod -R ug+rwx storage bootstrap/cache
+
+echo "[📦] Instalando dependências do Laravel..."
 composer install --no-interaction
 
 if grep -q "APP_KEY=" .env && [ -z "$(grep APP_KEY= .env | cut -d '=' -f2)" ]; then
-  echo "[??] Gerando APP_KEY..."
+  echo "[🔐] Gerando APP_KEY..."
   php artisan key:generate
 fi
 
-echo "[??] Configurando o ambiente..."
+echo "[🌐] Configurando o ambiente..."
 php artisan session:table
 
-echo "[???] Rodando migrations..."
+echo "[🗃️] Rodando migrations..."
 php artisan migrate --force
 
-echo "[??] Criando storage:link..."
+echo "[🔗] Criando storage:link..."
 php artisan storage:link
 
-echo "[??] Cacheando configura��es..."
+echo "[📂] Cacheando configurações..."
 php artisan config:cache
 
-echo "[?] Laravel pronto!"
+echo "[✅] Laravel pronto!"
 php-fpm
