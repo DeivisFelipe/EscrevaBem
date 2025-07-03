ef6fedc3889f4a735029ae6e2287ae7812536ea1
DeivisFelipe <guerreirodosul01@gmail.com>
2025-07-03 17:53:49 -0300

    feat: colocando o título no header

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="bg-primary text-neutral-light p-4 shadow-md">
     <div class="container mx-auto flex justify-between items-center">
-        <h1 class="text-2xl font-bold">Meu Site</h1>
+        <h1 class="text-2xl font-bold">Escreva Bem</h1>
         <nav>
         <ul class="flex space-x-6">
             <li><a href="#" class="hover:underline hover:text-accent">Home</a></li>
