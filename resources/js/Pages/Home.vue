6a63bfdad6f67abebd151ee4ce679add483748c5
DeivisFelipe <guerreirodosul01@gmail.com>
2025-07-03 16:09:11 -0300

    feat: resto do tailwind

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1>Olá, Inertia!</h1>
+  <div>
+    <Header />
+    <main class="p-8">
+      <h2 class="text-xl font-semibold mb-4">Conteúdo da página</h2>
+      <p>Este é um texto de exemplo para testar Tailwind CSS.</p>
+    </main>
+  </div>
 </template>
+
+<script>
+import Header from '../Components/Header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
