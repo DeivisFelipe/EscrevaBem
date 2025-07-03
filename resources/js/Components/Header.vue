a990327de5dd4a760f8b6a0e8e7e483ccb4f4532
DeivisFelipe <guerreirodosul01@gmail.com>
2025-07-03 16:48:08 -0300

    feat: fazendo o tema funcionar

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -1,17 +1,17 @@
 <template>
-  <header class="bg-blue-600 text-white p-4 shadow-md">
+  <header class="bg-primary text-neutral-light p-4 shadow-md">
     <div class="container mx-auto flex justify-between items-center">
-      <h1 class="text-2xl font-bold">Meu Site</h1>
-      <nav>
+        <h1 class="text-2xl font-bold">Meu Site</h1>
+        <nav>
         <ul class="flex space-x-6">
-          <li><a href="#" class="hover:underline hover:text-blue-200">Home</a></li>
-          <li><a href="#" class="hover:underline hover:text-blue-200">Sobre</a></li>
-          <li><a href="#" class="hover:underline hover:text-blue-200">Serviços</a></li>
-          <li><a href="#" class="hover:underline hover:text-blue-200">Contato</a></li>
+            <li><a href="#" class="hover:underline hover:text-accent">Home</a></li>
+            <li><a href="#" class="hover:underline hover:text-accent">Sobre</a></li>
+            <li><a href="#" class="hover:underline hover:text-accent">Serviços</a></li>
+            <li><a href="#" class="hover:underline hover:text-accent">Contato</a></li>
         </ul>
-      </nav>
+        </nav>
     </div>
-  </header>
+    </header>
 </template>
 
 <script>
@@ -19,7 +19,3 @@ export default {
   name: 'Header',
 }
 </script>
-
-<style scoped>
-/* Não precisa, Tailwind cuida do estilo */
-</style>
