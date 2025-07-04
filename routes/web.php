48f92ef330adbb7e474f710f75e3dc6fc8541e89
DeivisFelipe <guerreirodosul01@gmail.com>
2025-07-04 10:30:40 -0300

    feat: adicionando itens do passport

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 Route::get('/sobre', function () {
     return Inertia::render('Sobre');
@@ -9,4 +11,17 @@ Route::get('/sobre', function () {
 
 Route::get('/', function () {
     return Inertia::render('Home');
+});
+
+Route::post('/login', function (Request $request) {
+    $response = Http::asForm()->post(env('APP_URL') . '/oauth/token', [
+        'grant_type' => 'password',
+        'client_id' => env('PASSWORD_CLIENT_ID'),
+        'client_secret' => env('PASSWORD_CLIENT_SECRET'),
+        'username' => $request->email,
+        'password' => $request->password,
+        'scope' => '',
+    ]);
+
+    return $response->json();
 });
