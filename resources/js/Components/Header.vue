7d519d4ac6801969be501e6a57b24abe09bdb2a7
DeivisFelipe <guerreirodosul01@gmail.com>
2025-07-04 14:09:43 -0300

    feat: colocando o logo

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -1,13 +1,13 @@
 <template>
-  <header class="bg-primary text-neutral-light p-4 shadow-md">
+  <header class="bg-primary text-neutral-light p-4 py-0 shadow-md">
     <div class="container mx-auto flex justify-between items-center">
-        <h1 class="text-2xl font-bold">Escreva Bem</h1>
+        <img src="../imagens/logo.png" alt="Logo" class="w-50 h-auto">
         <nav>
         <ul class="flex space-x-6">
-            <li><a href="#" class="hover:underline hover:text-accent">Home</a></li>
-            <li><a href="#" class="hover:underline hover:text-accent">Sobre</a></li>
-            <li><a href="#" class="hover:underline hover:text-accent">Serviços</a></li>
-            <li><a href="#" class="hover:underline hover:text-accent">Contato</a></li>
+            <li><a href="#" class="hover:underline">Home</a></li>
+            <li><a href="#" class="hover:underline">Sobre</a></li>
+            <li><a href="#" class="hover:underline">Serviços</a></li>
+            <li><a href="#" class="hover:underline">Contato</a></li>
         </ul>
         </nav>
     </div>
